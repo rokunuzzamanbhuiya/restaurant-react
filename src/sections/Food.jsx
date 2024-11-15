6652cf6caf0dcd092c79771a07d93f0ec63e4cd9
Rokunuzzaman Bhuiya <rokunuzzamanbhuiya@gmail.com>
2024-11-15 19:21:22 +0600

    swiper slider added

--- src/sections/Food.jsx
+++ src/sections/Food.jsx
@@ -1,93 +1,193 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+import SwiperCore from "swiper";
+import { useRef } from "react";
+
+// import required modules
+import { Autoplay, Navigation } from "swiper/modules";
 
 const Food = () => {
-    return (
-        <>
-            <section id="food" className="ts-food-area pt_120 pb_120">
-            <img className="ts-food-bg-img" src="assets/img/food/food-bg-img.png" alt="Restaurant" />
-            <div className="container">
-                <div className="row">
-                <div className="col-xl-6">
-                    <div className="ts-section-title-wrapper mb_60">
-                    <span className="ts-section-subtitle">Crispy, Every Bite
-                        Taste
-                    </span>
-                    <h2 className="ts-section-title">POPULAR FOOD ITEMS</h2>
-                    </div>
-                </div>
-                <div className="col-xl-6">
-                    <div className="ts-food-slider-arrow">
-                    <div className="ts-swiper-food-button-prev">
-                        <i className="fas fa-chevron-left" />
-                    </div>
-                    <div className="ts-swiper-food-button-next">
-                        <i className="fas fa-chevron-right" />
-                    </div>
-                    </div>
-                </div>
-                </div>
+  SwiperCore.use([Navigation]);
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
+  return (
+    <>
+      <section id="food" className="ts-food-area pt_120 pb_120">
+        <img
+          className="ts-food-bg-img"
+          src="assets/img/food/food-bg-img.png"
+          alt="Restaurant"
+        />
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-6">
+              <div className="ts-section-title-wrapper mb_60">
+                <span className="ts-section-subtitle">
+                  Crispy, Every Bite Taste
+                </span>
+                <h2 className="ts-section-title">POPULAR FOOD ITEMS</h2>
+              </div>
             </div>
-            <div className="container">
-                <div className="row g-4">
-               
-                        <div className="col-xl-3 col-lg-6 col-md-12">
-                        <div className="tsfood text-center">
-                        <div className="tsfood__thumb mb_20">
-                            <img src="assets/img/food/food-img-1.png" alt="Restaurant" />
-                        </div>
-                        <div className="tsfood__shape">
-                        </div>
-                        <div className="tsfood__content mt_30">
-                            <h4><a href="#">vegetables burger</a></h4>
-                            <span>Barbecue Italian cuisine pizza</span>
-                        </div>
-                        </div>
-                    </div>
-                    <div className="col-xl-3 col-lg-6 col-md-12">
-                        <div className="tsfood text-center">
-                        <div className="tsfood__thumb mb_30">
-                            <img src="assets/img/food/food-img-2.png" alt="Restaurant" />
-                        </div>
-                        <div className="tsfood__shape">
-                        </div>
-                        <div className="tsfood__content mt_20">
-                            <h4><a href="#">Spacial Pizza</a></h4>
-                            <span>Barbecue Italian cuisine pizza</span>
-                        </div>
-                        </div>
-                    </div>
-                    <div className="col-xl-3 col-lg-6 col-md-12">
-                        <div className="tsfood text-center">
-                        <div className="tsfood__thumb mb_30">
-                            <img src="assets/img/food/food-img-3.png" alt="Restaurant" />
-                        </div>
-                        <div className="tsfood__shape">
-                        </div>
-                        <div className="tsfood__content mt_20">
-                            <h4><a href="#">Spacial French fries</a></h4>
-                            <span>Barbecue Italian cuisine</span>
-                        </div>
-                        </div>
-                    </div>
-                    <div className="col-xl-3 col-lg-6 col-md-12">
-                        <div className="tsfood text-center">
-                        <div className="tsfood__thumb mb_30">
-                            <img src="assets/img/food/food-img-4.png" alt="Restaurant" />
-                        </div>
-                        <div className="tsfood__shape">
-                        </div>
-                        <div className="tsfood__content mt_20">
-                            <h4><a href="#">Cuisine Chicken</a></h4>
-                            <span>Japanese Cuisine Chicken</span>
-                        </div>
-                        </div>
-                    </div>
-                    
+            <div className="col-xl-6">
+              <div className="ts-food-slider-arrow">
+                <div className="ts-swiper-food-button-prev" ref={prevRef}>
+                  <i className="fas fa-chevron-left" />
                 </div>
+                <div className="ts-swiper-food-button-next" ref={nextRef}>
+                  <i className="fas fa-chevron-right" />
+                </div>
+              </div>
             </div>
-            </section>
-        </>
-    );
+          </div>
+        </div>
+        <div className="container">
+          <div className="row g-4">
+            <Swiper
+              onInit={(swiper) => {
+                swiper.params.navigation.prevEl = prevRef.current;
+                swiper.params.navigation.nextEl = nextRef.current;
+                swiper.navigation.init();
+                swiper.navigation.update();
+              }}
+              breakpoints={{
+                320: {
+                  slidesPerView: 1,
+                },
+                640: {
+                  slidesPerView: 1,
+                },
+                768: {
+                  slidesPerView: 2,
+                },
+                1024: {
+                  slidesPerView: 4,
+                },
+              }}
+              slidesPerView={4}
+              spaceBetween={30}
+              navigation={false}
+              modules={[Autoplay, Navigation]}
+              autoplay={{ delay: 5000 }}
+              className="mySwiper"
+            >
+              <SwiperSlide>
+                <div className="tsfood text-center">
+                  <div className="tsfood__thumb mb_20">
+                    <img
+                      src="assets/img/food/food-img-1.png"
+                      alt="Restaurant"
+                    />
+                  </div>
+                  <div className="tsfood__shape"></div>
+                  <div className="tsfood__content mt_30">
+                    <h4>
+                      <a href="#">vegetables burger</a>
+                    </h4>
+                    <span>Barbecue Italian cuisine pizza</span>
+                  </div>
+                </div>
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <div className="tsfood text-center">
+                  <div className="tsfood__thumb mb_30">
+                    <img
+                      src="assets/img/food/food-img-2.png"
+                      alt="Restaurant"
+                    />
+                  </div>
+                  <div className="tsfood__shape"></div>
+                  <div className="tsfood__content mt_20">
+                    <h4>
+                      <a href="#">Spacial Pizza</a>
+                    </h4>
+                    <span>Barbecue Italian cuisine pizza</span>
+                  </div>
+                </div>
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <div className="tsfood text-center">
+                  <div className="tsfood__thumb mb_30">
+                    <img
+                      src="assets/img/food/food-img-3.png"
+                      alt="Restaurant"
+                    />
+                  </div>
+                  <div className="tsfood__shape"></div>
+                  <div className="tsfood__content mt_20">
+                    <h4>
+                      <a href="#">Spacial French fries</a>
+                    </h4>
+                    <span>Barbecue Italian cuisine</span>
+                  </div>
+                </div>
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <div className="tsfood text-center">
+                  <div className="tsfood__thumb mb_30">
+                    <img
+                      src="assets/img/food/food-img-4.png"
+                      alt="Restaurant"
+                    />
+                  </div>
+                  <div className="tsfood__shape"></div>
+                  <div className="tsfood__content mt_20">
+                    <h4>
+                      <a href="#">Cuisine Chicken</a>
+                    </h4>
+                    <span>Japanese Cuisine Chicken</span>
+                  </div>
+                </div>
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <div className="tsfood text-center">
+                  <div className="tsfood__thumb mb_30">
+                    <img
+                      src="assets/img/food/food-img-2.png"
+                      alt="Restaurant"
+                    />
+                  </div>
+                  <div className="tsfood__shape"></div>
+                  <div className="tsfood__content mt_20">
+                    <h4>
+                      <a href="#">Spacial Pizza</a>
+                    </h4>
+                    <span>Barbecue Italian cuisine pizza</span>
+                  </div>
+                </div>
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <div className="tsfood text-center">
+                  <div className="tsfood__thumb mb_30">
+                    <img
+                      src="assets/img/food/food-img-3.png"
+                      alt="Restaurant"
+                    />
+                  </div>
+                  <div className="tsfood__shape"></div>
+                  <div className="tsfood__content mt_20">
+                    <h4>
+                      <a href="#">Spacial French fries</a>
+                    </h4>
+                    <span>Barbecue Italian cuisine</span>
+                  </div>
+                </div>
+              </SwiperSlide>
+            </Swiper>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Food;
--- src/sections/Testimonial.jsx
+++ src/sections/Testimonial.jsx
@@ -1,140 +1,235 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import VideoPopup from '../components/VideoPopup'
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-creative";
+
+// import required modules
+import { Autoplay, Navigation, EffectCreative } from "swiper/modules";
+
+import SwiperCore from "swiper";
+import { useRef } from "react";
+
+import VideoPopup from "../components/VideoPopup";
 
 const Testimonial = () => {
-    return (
-        <>
-          <section id="testimonial" className="ts-testimonial-area pt_120 pb_120">
-            <img className="ts-testimonial-bg-img1" src="assets/img/testimonial/testimonial-bg-img1.png" alt="Restaurant" />
-            <img className="ts-testimonial-bg-img2" src="assets/img/testimonial/testimonial-bg-img2.png" alt="Restaurant" />
-            <div className="container">
-              <div className="row">
-                <div className="col-xl-8">
-                  <div className="ts-section-title-wrapper mb_60">
-                    <span className="ts-section-subtitle">Customers Testimonial
+  SwiperCore.use([Navigation]);
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
+  return (
+    <>
+      <section id="testimonial" className="ts-testimonial-area pt_120 pb_120">
+        <img
+          className="ts-testimonial-bg-img1"
+          src="assets/img/testimonial/testimonial-bg-img1.png"
+          alt="Restaurant"
+        />
+        <img
+          className="ts-testimonial-bg-img2"
+          src="assets/img/testimonial/testimonial-bg-img2.png"
+          alt="Restaurant"
+        />
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-8">
+              <div className="ts-section-title-wrapper mb_60">
+                <span className="ts-section-subtitle">
+                  Customers Testimonial
+                </span>
+                <h2 className="ts-section-title">
+                  What Some of my Customers Say
+                </h2>
+              </div>
+            </div>
+            <div className="col-xl-4">
+              <div className="ts-testimonial-slider-arrow">
+                <div
+                  className="ts-swiper-testimonial-button-prev"
+                  ref={prevRef}
+                >
+                  <i className="fas fa-chevron-left" />
+                </div>
+                <div
+                  className="ts-swiper-testimonial-button-next"
+                  ref={nextRef}
+                >
+                  <i className="fas fa-chevron-right" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="container">
+          <Swiper
+            onInit={(swiper) => {
+              swiper.params.navigation.prevEl = prevRef.current;
+              swiper.params.navigation.nextEl = nextRef.current;
+              swiper.navigation.init();
+              swiper.navigation.update();
+            }}
+            modules={[Autoplay, EffectCreative]}
+            autoplay={{ delay: 4000 }}
+            grabCursor={true}
+            effect={"creative"}
+            creativeEffect={{
+              prev: {
+                shadow: true,
+                origin: "left center",
+                translate: ["-5%", 0, -200],
+                rotate: [0, 100, 0],
+              },
+              next: {
+                origin: "right center",
+                translate: ["5%", 0, -200],
+                rotate: [0, -100, 0],
+              },
+            }}
+            className="mySwiper6"
+          >
+            <SwiperSlide>
+              <div className="row g-0 flex-row-reverse">
+                <div className="col-xl-7">
+                  <div className="ts-testimonial-thumb">
+                    <img
+                      src="assets/img/testimonial/testimonial-bg-video1.jpg"
+                      alt="Restaurant"
+                    />
+                    <VideoPopup />
+                  </div>
+                </div>
+                <div className="col-xl-5">
+                  <div className="ts-testimonial-content">
+                    <img
+                      className="ts-testimonial-quote"
+                      src="assets/img/testimonial/quote.png"
+                      alt="Restaurant"
+                    />
+                    <span>
+                      You can&apos;t go wrong with Chicken Mandi, I had it
+                      twice. The chicken was cooked perfectly, juicy &amp; soft
+                      (usually mandi chicken is a bit dry). I would defiantly
+                      recommend it.
                     </span>
-                    <h2 className="ts-section-title">What Some of my Customers Say</h2>
-                  </div>
-                </div>
-                <div className="col-xl-4">
-                  <div className="ts-testimonial-slider-arrow">
-                    <div className="ts-swiper-testimonial-button-prev">
-                      <i className="fas fa-chevron-left" />
-                    </div>
-                    <div className="ts-swiper-testimonial-button-next">
-                      <i className="fas fa-chevron-right" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="container">
-              <div className="swiper tsTestimonialSwiper">
-                <div className="swiper-wrapper">
-                  <div className="swiper-slide">
-                    <div className="row g-0 flex-row-reverse">
-                      <div className="col-xl-7">
-                        <div className="ts-testimonial-thumb">
-                          <img src="assets/img/testimonial/testimonial-bg-video1.jpg" alt="Restaurant" />
-                            <VideoPopup />
-                        </div>
+                    <div className="ts-testimonial-info d-flex align-items-center justify-content-between mt_184">
+                      <div className="ts-testimonial-name-desig">
+                        <h5>Khalid Al Dawsry</h5>
+                        <span>Jeddah, Saudi</span>
                       </div>
-                      <div className="col-xl-5">
-                        <div className="ts-testimonial-content">
-                          <img className="ts-testimonial-quote" src="assets/img/testimonial/quote.png" alt="Restaurant" />
-                          <span>
-                            You can&apos;t go wrong with Chicken Mandi, I had it twice. The chicken was
-                            cooked
-                            perfectly,
-                            juicy &amp; soft (usually mandi
-                            chicken is a bit dry). I would defiantly recommend it.
-                          </span>
-                          <div className="ts-testimonial-info d-flex align-items-center justify-content-between mt_184">
-                            <div className="ts-testimonial-name-desig">
-                              <h5>Khalid Al Dawsry</h5>
-                              <span>Jeddah, Saudi</span>
-                            </div>
-                            <div className="ts-testimonial-pic">
-                              <img src="assets/img/testimonial/testimonial-pic1.png" alt="Restaurant" />
-                            </div>
-                          </div>
-                          <img className="ts-testimonial-shape" src="assets/img/testimonial/testimonial-shape.svg" alt="Restaurant" />
-                        </div>
+                      <div className="ts-testimonial-pic">
+                        <img
+                          src="assets/img/testimonial/testimonial-pic1.png"
+                          alt="Restaurant"
+                        />
                       </div>
                     </div>
-                  </div>
-                  <div className="swiper-slide">
-                    <div className="row g-0 flex-row-reverse">
-                      <div className="col-xl-7">
-                        <div className="ts-testimonial-thumb">
-                          <img src="assets/img/testimonial/testimonial-bg-video2.jpg" alt="Restaurant" />
-                            <VideoPopup />
-                        </div>
-                      </div>
-                      <div className="col-xl-5">
-                        <div className="ts-testimonial-content">
-                          <img className="ts-testimonial-quote" src="assets/img/testimonial/quote.png" alt="Restaurant" />
-                          <span>
-                            I would defiantly recommend it. You can&apos;t go wrong with Chicken Mandi, I had
-                            it twice. The chicken was
-                            cooked
-                            perfectly,
-                            juicy &amp; soft (usually mandi
-                            chicken is a bit dry).
-                          </span>
-                          <div className="ts-testimonial-info d-flex align-items-center justify-content-between mt_184">
-                            <div className="ts-testimonial-name-desig">
-                              <h5>Malida Ml Pawsry</h5>
-                              <span>Jeddah, Saudi</span>
-                            </div>
-                            <div className="ts-testimonial-pic">
-                              <img src="assets/img/testimonial/testimonial-pic2.png" alt="Restaurant" />
-                            </div>
-                          </div>
-                          <img className="ts-testimonial-shape" src="assets/img/testimonial/testimonial-shape.svg" alt="Restaurant" />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="swiper-slide">
-                    <div className="row g-0 flex-row-reverse">
-                      <div className="col-xl-7">
-                        <div className="ts-testimonial-thumb">
-                          <img src="assets/img/testimonial/testimonial-bg-video3.jpg" alt="Restaurant" />
-                            <VideoPopup />
-                        </div>
-                      </div>
-                      <div className="col-xl-5">
-                        <div className="ts-testimonial-content">
-                          <img className="ts-testimonial-quote" src="assets/img/testimonial/quote.png" alt="Restaurant" />
-                          <span>
-                            The chicken was
-                            cooked
-                            perfectly,
-                            juicy &amp; soft (usually mandi
-                            chicken is a bit dry). You can&apos;t go wrong with Chicken Mandi, I had it
-                            twice. I would defiantly recommend it.
-                          </span>
-                          <div className="ts-testimonial-info d-flex align-items-center justify-content-between mt_184">
-                            <div className="ts-testimonial-name-desig">
-                              <h5>Whalid Hl Nawsry</h5>
-                              <span>Jeddah, Saudi</span>
-                            </div>
-                            <div className="ts-testimonial-pic">
-                              <img src="assets/img/testimonial/testimonial-pic3.png" alt="Restaurant" />
-                            </div>
-                          </div>
-                          <img className="ts-testimonial-shape" src="assets/img/testimonial/testimonial-shape.svg" alt="Restaurant" />
-                        </div>
-                      </div>
-                    </div>
+                    <img
+                      className="ts-testimonial-shape"
+                      src="assets/img/testimonial/testimonial-shape.svg"
+                      alt="Restaurant"
+                    />
                   </div>
                 </div>
               </div>
-            </div>
-          </section>
-        </>
-    );
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <div className="row g-0 flex-row-reverse">
+                <div className="col-xl-7">
+                  <div className="ts-testimonial-thumb">
+                    <img
+                      src="assets/img/testimonial/testimonial-bg-video2.jpg"
+                      alt="Restaurant"
+                    />
+                    <VideoPopup />
+                  </div>
+                </div>
+                <div className="col-xl-5">
+                  <div className="ts-testimonial-content">
+                    <img
+                      className="ts-testimonial-quote"
+                      src="assets/img/testimonial/quote.png"
+                      alt="Restaurant"
+                    />
+                    <span>
+                      I would defiantly recommend it. You can&apos;t go wrong
+                      with Chicken Mandi, I had it twice. The chicken was cooked
+                      perfectly, juicy &amp; soft (usually mandi chicken is a
+                      bit dry).
+                    </span>
+                    <div className="ts-testimonial-info d-flex align-items-center justify-content-between mt_184">
+                      <div className="ts-testimonial-name-desig">
+                        <h5>Malida Ml Pawsry</h5>
+                        <span>Jeddah, Saudi</span>
+                      </div>
+                      <div className="ts-testimonial-pic">
+                        <img
+                          src="assets/img/testimonial/testimonial-pic2.png"
+                          alt="Restaurant"
+                        />
+                      </div>
+                    </div>
+                    <img
+                      className="ts-testimonial-shape"
+                      src="assets/img/testimonial/testimonial-shape.svg"
+                      alt="Restaurant"
+                    />
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <div className="row g-0 flex-row-reverse">
+                <div className="col-xl-7">
+                  <div className="ts-testimonial-thumb">
+                    <img
+                      src="assets/img/testimonial/testimonial-bg-video3.jpg"
+                      alt="Restaurant"
+                    />
+                    <VideoPopup />
+                  </div>
+                </div>
+                <div className="col-xl-5">
+                  <div className="ts-testimonial-content">
+                    <img
+                      className="ts-testimonial-quote"
+                      src="assets/img/testimonial/quote.png"
+                      alt="Restaurant"
+                    />
+                    <span>
+                      The chicken was cooked perfectly, juicy &amp; soft
+                      (usually mandi chicken is a bit dry). You can&apos;t go
+                      wrong with Chicken Mandi, I had it twice. I would
+                      defiantly recommend it.
+                    </span>
+                    <div className="ts-testimonial-info d-flex align-items-center justify-content-between mt_184">
+                      <div className="ts-testimonial-name-desig">
+                        <h5>Whalid Hl Nawsry</h5>
+                        <span>Jeddah, Saudi</span>
+                      </div>
+                      <div className="ts-testimonial-pic">
+                        <img
+                          src="assets/img/testimonial/testimonial-pic3.png"
+                          alt="Restaurant"
+                        />
+                      </div>
+                    </div>
+                    <img
+                      className="ts-testimonial-shape"
+                      src="assets/img/testimonial/testimonial-shape.svg"
+                      alt="Restaurant"
+                    />
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Testimonial;
